a781a9ae07ba1b7e3eaf8105de9dde00f19a6a89
Langtao <1419426643@qq.com>
2021-01-23 00:13:21 +0800

    List

---
+++ Algorithm/src/List/DeleteNode.java
@@ -0,0 +1,12 @@
+package List;/**
+*
+*@ClassName DeleteNode
+*@Description TODO
+*@Author Langtao
+*@Date 2021/1/22 22:58
+*@Version V1.0
+*
+*/
+
+public class DeleteNode {
+}
---
+++ Algorithm/src/List/DoubleNode.java
@@ -0,0 +1,12 @@
+package List;
+
+/**
+ * @ClassName Node
+ * @Description TODO
+ * @Author Langtao
+ * @Date 2021/1/22 22:59
+ * @Version V1.0
+ */
+
+public class Node {
+}
