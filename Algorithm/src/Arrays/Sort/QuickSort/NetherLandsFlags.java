d023f868ceef7bf98d3d06e32fb998c7049f5ce4
clt19960101 <37727896+clt19960101@users.noreply.github.com>
2021-02-13 18:25:48 +0800

    Algorithm-荷兰国旗问题

--- Algorithm/src/Arrays/Sort/QuickSort/NetherLandsFlags.java
+++ Algorithm/src/Arrays/Sort/QuickSort/NetherLandsFlags.java
@@ -57,8 +57,11 @@ public class NetherLandsFlags {
     }
 
     private static void swap(int[] arr, int index1, int index2) {
-        arr[index1] = arr[index1] ^ arr[index2];
-        arr[index2] = arr[index1] ^ arr[index2];
-        arr[index1] = arr[index1] ^ arr[index2];
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+//        arr[index1] = arr[index1] ^ arr[index2];
+//        arr[index2] = arr[index1] ^ arr[index2];
+//        arr[index1] = arr[index1] ^ arr[index2];
     }
 }
