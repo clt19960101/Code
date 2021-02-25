7fc9ea129dd3847ccd85492910065941f5788e03
clt19960101 <37727896+clt19960101@users.noreply.github.com>
2021-02-25 12:15:53 +0800

    Alogrithm-大数堆、堆排序

--- Algorithm/src/Heap/HeapSort.java
+++ Algorithm/src/Heap/HeapSort.java
@@ -1,64 +1,94 @@
 package Heap;
 
-import java.util.Iterator;
-import java.util.PriorityQueue;
-
 /**
  * @ClassName HeapSort
- * @Description 堆排序
- * @Author Langtao
- * @Date 2021/2/23 23:21
- * @Version V1.0
+ * @Author Chen Langtao
+ * @Date 2021/2/15 17:17
+ * @Description 堆排序 1）组织成大根堆 2）排序
+ * @Version 1.0
  */
-
 public class HeapSort {
     public static void main(String[] args) {
-        //系统默认是小根堆
-//        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
-//        priorityQueue.add(5);
-//        priorityQueue.add(3);
-//        priorityQueue.add(2);
-//        priorityQueue.add(6);
-//        priorityQueue.add(7);
-//        Iterator<Integer> iterator = priorityQueue.iterator();
-//        while (iterator.hasNext()) {
-//            Integer next = iterator.next();
-//            System.out.println(next);
-//        }
-        int[] arr = new int[]{1, 2, 4, 2, 6, 4, 7, 8, 9, 5, 3};
-        //每个元素移动的距离一定不超过K
-        int k = 4;
-        sortArrDisLessK(arr, k);
+        int[] arr = {12, 23, 4, 2, 4, 53, 3};
+        doHeapSort(arr);
+    }
+
+    //额外空间复杂度O(1)
+    private static void doHeapSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        //法一和法二只是把数组调整成大根堆
+        //方法一：数组变为大根堆的代价：O(N*logN) 一个数一个数的放
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+            heapInert(arr, i);
+        }
+        //方法二：数组变为大根堆的代价：O(N)
+        for (int i = arr.length - 1; i >= 0; i--) {
+            heapify(arr, i, arr.length);
+        }
+        //排序工作
+        int heapSize = arr.length;
+        swap(arr, 0, --heapSize);
+        //算法时间复杂度O(logN*N)
+        while (heapSize > 0) {//O(N)
+            heapify(arr, 0, heapSize);//O(logN)
+            swap(arr, 0, --heapSize);//O(1)
+        }
+
+
+    }
+
+    /**
+     * @Name heapify
+     * @Param [arr, i, length]
+     * @Retrun void
+     * @Author Chen Langtao
+     * @Date 2021/2/15 17:43
+     * @Throws
+     * @Description 从右往左不断下沉
+     */
+    private static void heapify(int[] arr, int index, int heapSize) {
+        //左孩子的索引
+        int leftIndex = index * 2 + 1;
+        while (leftIndex < heapSize) {
+            //左右子节点值较大的一个的索引
+            int largest = leftIndex + 1 < heapSize && arr[leftIndex + 1] > arr[leftIndex] ? leftIndex + 1 : leftIndex;
+            //较大节点与当前父亲节点较大的一个
+            largest = arr[index] > arr[largest] ? index : largest;
+            if (largest == index) {
+                break;
+            }
+            swap(arr, index, largest);
+            index = largest;
+            //继续下沉
+            leftIndex = index * 2 + 1;
         }
 
     }
-    /**
-     * @Author Langtao
-     * @Description 已知一个几乎有序的数组。几乎有序是指，如果把数组排好序的话，每个元素移动的距离一定不超过K,
-     * 并且K相对于数组长度来说是比较小的。
-     * @Date 23:36 2021/2/23
-     * @Param [arr, k]
-     * @return void
-     */
-    private static void sortArrDisLessK(int[] arr, int k) {
-        //默认小根堆
-        PriorityQueue<Object> heap = new PriorityQueue<>();
-        int index = 0;
-        //0..K
-        for (; index < Math.min(arr.length - 1, k); index++) {
-            heap.add(arr[index]);
-        }
-        int i = 0;
-        for (; index < arr.length; i++, index++) {
-            arr[i] = (int)heap.poll();
-            heap.add(arr[index]);
-        }
-        while (!heap.isEmpty()) {
-            arr[i++] = (int)heap.poll();
-        }
 
+    /**
+     * @Name
+     * @Param
+     * @Retrun
+     * @Author Chen Langtao
+     * @Date 2021/2/15 17:39
+     * @Throws
+     * @Description 法一：一个数一个数的组成为大根堆
+     */
+    private static void heapInert(int[] arr, int i) {
+        //找到父节点
+        while (arr[i] > arr[(i - 1) / 2]) {
+            swap(arr, i, (i - 1) / 2);
+            //继续向上比较父节点
+            i = (i - 1) / 2;
+        }
+    }
+
+    private static void swap(int[] heap, int index, int i) {
+        int temp = heap[index];
+        heap[index] = heap[i];
+        heap[i] = temp;
     }
 
 }
--- Algorithm/src/Heap/MaxHeap.java
+++ Algorithm/src/Heap/MaxHeap.java
@@ -1,5 +1,7 @@
 package Heap;
 
+import java.util.PriorityQueue;
+
 /**
  * @ClassName MaxHeap
  * @Author Chen Langtao
@@ -11,8 +13,27 @@ package Heap;
  */
 public class MaxHeap {
     public static void main(String[] args) {
+        //系统默认是小根堆
+//        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+//        priorityQueue.add(5);
+//        priorityQueue.add(3);
+//        priorityQueue.add(2);
+//        priorityQueue.add(6);
+//        priorityQueue.add(7);
+//        Iterator<Integer> iterator = priorityQueue.iterator();
+//        while (iterator.hasNext()) {
+//            Integer next = iterator.next();
+//            System.out.println(next);
+//        }
+        int[] arr = new int[]{1, 2, 4, 2, 6, 4, 7, 8, 9, 5, 3};
+        //每个元素移动的距离一定不超过K
+        int k = 4;
+        sortArrDisLessK(arr, k);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
         //将给定的数组组织成大根堆
-        int[] arr = new int[]{7, 4, 2, 3, 2, 1};
         MyMaxHeap maxHeap = new MyMaxHeap(5);
         maxHeap.push(2);
         maxHeap.push(4);
@@ -21,6 +42,33 @@ public class MaxHeap {
         maxHeap.push(7);
         System.out.println(maxHeap.pop());
         System.out.println(maxHeap.pop());
+    }
+
+    /**
+     * @Author Langtao
+     * @Description 已知一个几乎有序的数组。几乎有序是指，如果把数组排好序的话，每个元素移动的距离一定不超过K,
+     * 并且K相对于数组长度来说是比较小的。
+     * @Date 23:36 2021/2/23
+     * @Param [arr, k]
+     * @return void
+     */
+    private static void sortArrDisLessK(int[] arr, int k) {
+        //默认小根堆
+        PriorityQueue<Object> heap = new PriorityQueue<>();
+        int index = 0;
+        //0..K
+        for (; index < Math.min(arr.length - 1, k); index++) {
+            heap.add(arr[index]);
+        }
+        int i = 0;
+        for (; index < arr.length; i++, index++) {
+            arr[i] = (int)heap.poll();
+            heap.add(arr[index]);
+        }
+        while (!heap.isEmpty()) {
+            arr[i++] = (int)heap.poll();
+        }
+
     }
 
     public static class MyMaxHeap {
