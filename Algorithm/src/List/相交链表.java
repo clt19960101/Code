dcf5806d68985d0659915e04ff92ce13596e9a76
clt19960101 <37727896+clt19960101@users.noreply.github.com>
2021-05-06 16:14:43 +0800

    Alogrithm-双指针法

--- Algorithm/src/List/相交链表.java
+++ Algorithm/src/List/相交链表.java
@@ -10,7 +10,8 @@ import java.util.HashSet;
  * @Version 1.0
  */
 public class 相交链表 {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    //哈希法
+    public ListNode getIntersectionNode_hash(ListNode headA, ListNode headB) {
         HashSet<ListNode> set = new HashSet<>();
         while (headA != null) {
             set.add(headA);
@@ -23,5 +24,17 @@ public class 相交链表 {
             headB = headB.next;
         }
         return null;
+    }
+    //双指针法
+    public ListNode getIntersectionNode_point(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode A = headA, B = headB;
+        while (A != B) {
+            A = A != null ? A.next : headB;
+            B = B != null ? B.next : headA;
+        }
+        return A;
     }
 }
