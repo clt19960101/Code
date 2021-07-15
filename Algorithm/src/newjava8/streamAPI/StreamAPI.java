c19cc34e2122008fa92fc29d8683281764c676af
Langtao <1419426643@qq.com>
2021-07-15 23:25:37 +0800

    java基础-java8新特性

--- Algorithm/src/newjava8/streamAPI/StreamAPI.java
+++ Algorithm/src/newjava8/streamAPI/StreamAPI.java
@@ -7,7 +7,11 @@ import java.util.stream.Stream;
 
 /**
  * @ClassName StreamAPI
- * @Description 一.Stream的三个操作步骤
+ * @Description
+ * 一.Stream的三个操作步骤
+ * 1.创建stream
+ * 2.中间操作
+ * 3.终止操作
  * @Author Langtao
  * @Date 2021/7/4 20:56
  * @Version V1.0
@@ -33,6 +37,18 @@ public class StreamAPI {
         Stream.iterate(0,(x)->x+2).limit(10).forEach(System.out::println);
         //b.生成
         Stream.generate(() -> Math.random()).limit(10).forEach(System.out::println);
+
+    }
+
+    //中间操作
+
+    /**
+     * 筛选与切片
+     * filter--接收Lambda,从流中排除某些元素
+     * limit--截断流，使其元素不超过给定数量
+     * skip(n)--跳过元素，返回扔掉前n个元素的流，
+     */
+    public static void test1(){
 
     }
 }
